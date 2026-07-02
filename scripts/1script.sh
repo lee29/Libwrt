9b9acf22153bd1f90c38fd93996397643706cc38
lee29 <327208429@qq.com>
2026-07-03 01:27:17 +0800

    Update 1script.sh

--- scripts/1script.sh
+++ scripts/1script.sh
@@ -61,14 +61,14 @@ chmod +x package/xunlei
 
 
 # 移除 自带的tindy2013/subconverter核心库，替换为MetaCubeX/subconverter（支持vless）
-rm -rf feeds/packages/libs/{jpcre2,libcron,quickjspp,rapidjson,toml11}
-git_sparse_clone master https://github.com/Dawneng/openwrt-subconverter jpcre2 
-git_sparse_clone master https://github.com/Dawneng/openwrt-subconverter libcron 
-git_sparse_clone master https://github.com/Dawneng/openwrt-subconverter quickjspp 
-git_sparse_clone master https://github.com/Dawneng/openwrt-subconverter rapidjson
-git_sparse_clone master https://github.com/Dawneng/openwrt-subconverter toml11 
-mv -v {jpcre2,libcron,quickjspp,rapidjson,toml11} feeds/packages/libs
-chmod +x feeds/packages/libs/{jpcre2,libcron,quickjspp,rapidjson,toml11}
+# rm -rf feeds/packages/libs/{jpcre2,libcron,quickjspp,rapidjson,toml11}
+# git_sparse_clone master https://github.com/Dawneng/openwrt-subconverter jpcre2 
+# git_sparse_clone master https://github.com/Dawneng/openwrt-subconverter libcron 
+# git_sparse_clone master https://github.com/Dawneng/openwrt-subconverter quickjspp 
+# git_sparse_clone master https://github.com/Dawneng/openwrt-subconverter rapidjson
+# git_sparse_clone master https://github.com/Dawneng/openwrt-subconverter toml11 
+# mv -v {jpcre2,libcron,quickjspp,rapidjson,toml11} feeds/packages/libs
+# chmod +x feeds/packages/libs/{jpcre2,libcron,quickjspp,rapidjson,toml11}
 
 rm -rf feeds/packages/net/{sub-web,subconverter}
 git_sparse_clone master https://github.com/Dawneng/openwrt-subconverter sub-web
